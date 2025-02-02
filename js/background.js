7de65e27942d02d87521fb096945e2444e591c05
cowwabanga <114105506+cowwabanga@users.noreply.github.com>
2025-02-02 23:13:06 +0500

    Placeholder page now is more interesting

--- js/background.js
+++ js/background.js
@@ -1,8 +1,12 @@
+/*
+    2025, cowwabanga
+    DVD_logo.svg from https://en.m.wikipedia.org/wiki/File:DVD_logo.svg
+*/
 let xx = 0;
 let yy = 0;
 let hvel = 7; // horizontal velocity
 let vvel = 7; // vertical velocity
-let c_size = 100;
+let logo;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -10,17 +14,17 @@ function setup() {
     fill(128, 255, 255, 128);
     xx = windowWidth / 2;
     yy = windowHeight / 2;
+    logo = loadImage("../assets/DVD_logo.svg");
+    imageMode(CENTER);
 }
-
-
 
 function draw() {
     background(255);
-    ellipse(xx, yy, 100, 100);
+    image(logo, xx, yy);
     xx += hvel;
-    if (xx > windowWidth - c_size / 2 || xx < c_size / 2)
+    if (xx > windowWidth - logo.width / 2 || xx < logo.width / 2)
         hvel = -hvel;
     yy += vvel;
-    if (yy > windowHeight - c_size / 2 || yy < c_size / 2)
+    if (yy > windowHeight - logo.height / 2 || yy < logo.height / 2)
         vvel = -vvel;
 }
